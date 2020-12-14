a69e4a0756b67d19edf7e1a2ff48f5364a3f1f9a
XueSong <song.xue@seek-data.com>
2020-12-14 11:30:42 +0800

    应该可以了

--- spider.js
+++ spider.js
@@ -1,6 +1,7 @@
 const spiderWorker = require('./spider/run');
+const execWorker = require('./lib/execRun');
 
 //运行基本
 (async function () {
-    await spiderWorker.runJob();
+    await execWorker.runExec();
 })();
